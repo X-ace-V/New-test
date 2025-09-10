4bfa6e741da7ba2c329619f810db1d2f7a292433
X-ace-V <kumarvibhanshu21@gmail.com>
2025-09-10 18:41:54 +0530

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main() {
+    for (int i = 1; i <= 5; ++i) std::cout << i << ' ';
+    std::cout << '\n';
+    for (int i = 5; i >= 1; --i) std::cout << i << ' ';
+    return 0;
+}
